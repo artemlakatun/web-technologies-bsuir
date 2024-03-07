4d2759508570962b347ac8da87785becbc4a3bed
Artem_Lakatun <alakatuni@mail.ru>
2024-03-07 22:32:58 +0300

    feat: complete lesson1(web-site + form)

--- blocks/footer.php
+++ blocks/footer.php
@@ -1,5 +1,21 @@
 <footer class="footer">
-
+    <div class="footer_el">artem lakatun</div>
+    <div class="footer_el">2024</div>
+    <div class="footer_el">+375(44)751-70-78</div>
 </footer>
+<div class="modal">
+    <div class="modal__dialog">
+        <div class="modal__content">
+            <form action="#">
+                <div class="modal__close" data-close>&times;</div>
+                <div class="modal__title">Поставте вашу оценку!</div>
+                <input required placeholder="Ваше имя" name="name" type="text" class="modal__input">
+                <input required placeholder="Ваша оценка" name="evaluation" type="text" class="modal__input">
+                <button class="btn btn_dark btn_min">Отправить</button>
+            </form>
+        </div>
+    </div>
+</div>
+<script src="js/index.js"></script>
 </body>
 </html>
